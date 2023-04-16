a3926701a0672548efafa6c2d35d14c87b8aa61d
Brandon Gentile <gentile.brandon@gmail.com>
2023-04-15 23:22:06 -0400

    Add hover class

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const grid = document.querySelector(".container");
 
-function createDivs(num) {
+/**
+ * Create 16 by 16 grid
+ */
+function createDivs() {
   for (i = 0; i < 256; i++) {
     const div = document.createElement("div");
     div.classList.add("square");
+    div.addEventListener("mouseenter", () => {
+      addClass(div);
+    });
     grid.appendChild(div);
   }
 }
 createDivs();
 
-document.addEventListener("mouseenter");
+//function to add class to divs
+function addClass(el) {
+  el.classList.add("hover");
+  //add class to div
+}
