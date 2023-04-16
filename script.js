0e426f25d561f4f10a23ecc375e686d4b376ca11
Brandon Gentile <gentile.brandon@gmail.com>
2023-04-15 23:44:06 -0400

    Add user input

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ const grid = document.querySelector(".container");
 /**
  * Create 16 by 16 grid
  */
-function createDivs() {
-  for (i = 0; i < 256; i++) {
+function createDivs(num) {
+  const total = num * num;
+  const container = document.querySelector(".container");
+  container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+  for (i = 0; i < total; i++) {
     const div = document.createElement("div");
     div.classList.add("square");
+    console.log(typeof div);
     div.addEventListener("mouseenter", () => {
       addClass(div);
     });
@@ -15,8 +20,10 @@ function createDivs() {
 }
 createDivs();
 
-//function to add class to divs
+/**
+ * Adds hover class to grid boxes
+ * @param {object} el html element
+ */
 function addClass(el) {
   el.classList.add("hover");
-  //add class to div
 }
